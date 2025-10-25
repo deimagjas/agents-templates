d0f6d470070f8c92fa7dd2256a620e36420b2b98
Carlos Alberto  Argüello Ramírez <deimagjas@MacBook-Air-de-Carlos.local>
2025-10-24 20:56:54 -0500

    add: models for input and output formats

--- a2t/agent.py
+++ a2t/agent.py
@@ -1,14 +1,39 @@
 from google.adk.agents import LlmAgent
 from google.adk.models.lite_llm import LiteLlm
+from google.adk.runners import Runner
+from google.adk.sessions import InMemorySessionService
+from google.genai import types
+from .models import CityTimeInput, TimeResponse
+
+APP_NAME = "document_app"
+USER_ID = "user_123"
+SESSION_ID = "session_456"
 
 def get_current_time(city: str) -> dict:
     """Returns the current time in a specified city."""
     return {"status": "success", "city": city, "time": "10:30 AM"}
 
 root_agent = LlmAgent(
-    model=LiteLlm(model="bedrock/anthropic.claude-3-5-sonnet-20240620-v1:0"),
+    model=LiteLlm(model="bedrock/anthropic.claude-3-haiku-20240307-v1:0"),
     name='root_agent',
     description="Tells the current time in a specified city.",
     instruction="You are a helpful assistant that tells the current time in cities. Use the 'get_current_time' tool for this purpose.",
+    input_schema=CityTimeInput,
+    output_schema=TimeResponse,
     tools=[get_current_time],
 )
+
+
+session_service = InMemorySessionService()
+session = session_service.create_session(
+    app_name=APP_NAME,
+    user_id=USER_ID,
+    session_id=SESSION_ID
+)
+# Create runner
+runner = Runner(
+    agent=root_agent,
+    app_name=APP_NAME,
+    session_service=session_service
+)
+
